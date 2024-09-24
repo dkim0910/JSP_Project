7cac0eb8026433fa7d3fafb9afc3fc7bca45eed6
Beommm1330 <c26beom@gmail.com>
2024-09-24 15:06:33 +0900

    1차 골격완성

---
+++ order_form/order_form.js
@@ -0,0 +1,7 @@
+function openPopup() {
+    var popupURL = "https://usinsa.com/popup/coupon/apply";
+
+    var popupProperties = "width=600, height=400 ,scrollbars=yes";
+
+    window.open(popupURL, "Popup", popupProperties);
+}
