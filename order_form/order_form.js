08faccc49ed5b3aa3afc239873f1db05e66b4f31
Beommm1330 <c26beom@gmail.com>
2024-09-24 17:11:28 +0900

    버튼-링크연결
    뒤로가기/배송지변경/쿠폰사용/결제하기 링크 연결

--- order_form/order_form.js
+++ order_form/order_form.js
@@ -1,7 +1,24 @@
-function openPopup() {
-    var popupURL = "https://usinsa.com/popup/coupon/apply";
+function openPopup_AddressChange() {
+    var popupURL = "https://kh-academy.co.kr/login/login.kh";
 
     var popupProperties = "width=600, height=400 ,scrollbars=yes";
 
     window.open(popupURL, "Popup", popupProperties);
 }
+function openPopup_couponApply() {
+    var popupURL = "https://www.daum.net";
+
+    var popupProperties = "width=600, height=400 ,scrollbars=yes";
+
+    window.open(popupURL, "Popup", popupProperties);
+}
+function toggleOtherPayments(show) {
+    var otherPayments = document.getElementById('other-payment-options');
+    if (show) {
+        otherPayments.style.display = 'block';
+    } else {
+        otherPayments.style.display = 'none';
+    }
+}
+
+                
