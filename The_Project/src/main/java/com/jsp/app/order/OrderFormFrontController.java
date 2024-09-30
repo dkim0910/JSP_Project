2f700163ff769774309f427fd71db31e9c6a8768
Beommm1330 <c26beom@gmail.com>
2024-09-30 17:46:28 +0900

    orderfrontcontroller

--- The_Project/src/main/java/com/jsp/app/order/OrderFormFrontController.java
+++ The_Project/src/main/java/com/jsp/app/order/OrderFormFrontController.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("*.bo")
+@WebServlet("*.do")
 public class OrderFormFrontController extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req,
