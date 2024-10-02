3ac85fdfb1f5572e6cb3c61111fae5d69eb102c0
Jung ho Park <coool416@gmail.com>
2024-10-02 11:05:50 +0900

    주석처리

--- Project/src/main/java/com/kh/order/OrderFormAction.java
+++ Project/src/main/java/com/kh/order/OrderFormAction.java
@@ -3,10 +3,10 @@ package com.kh.order;
 import com.kh.login.UserDAO;
 import com.kh.web.action.Action;
 import com.kh.web.action.ActionForward;
-<<<<<<< HEAD
-
-=======
->>>>>>> branch 'main' of https://github.com/dkim0910/JSP_Project.git
+//<<<<<<< HEAD
+//
+//=======
+//>>>>>>> branch 'main' of https://github.com/dkim0910/JSP_Project.git
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
