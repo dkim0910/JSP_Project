8dd4d4542de540f2b19f9590579f9d55c8d1bfb5
son <seokyyy_@naver.com>
2024-10-02 12:21:32 +0900

    메인 프론트컨트롤러 작업중

--- Project/src/main/java/com/kh/main/MainForntContoller.java
+++ Project/src/main/java/com/kh/main/MainForntContoller.java
@@ -2,6 +2,8 @@ package com.kh.main;
 
 import java.io.IOException;
 
+import com.kh.login.UserDTO;
+import com.kh.mypage.action.MemberListAction;
 import com.kh.web.action.ActionForward;
 
 import jakarta.servlet.RequestDispatcher;
@@ -10,38 +12,30 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
-@WebServlet("*.ma")
+@WebServlet("*login")
 public class MainForntContoller extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.doGet(req, resp);
+		doProcess(req, resp);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.doPost(req, resp);
+		doProcess(req, resp);
 	}
 
 	protected void doProcess(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		String requestURI = req.getRequestURI();
+		String rquestURI = req.getRequestURI();
 		ActionForward forward = null;
 
-//		switch (requestURI) {
-//		case "":
-//			forward = new ().execute(req, resp);
-//			break;
-//		}
-
-		if (forward != null) {
-			if (forward.isRedirect()) {
-				resp.sendRedirect(forward.getPath());
-			} else {
-				RequestDispatcher disp = req.getRequestDispatcher(forward.getPath());
-				disp.forward(req, resp);
-			}
+		switch (rquestURI) {
+		case "/main-page/loginName.login":
+			forward = new nameAction().execute(req, resp);
+			break;
 		}
-
 	}
+
 }
