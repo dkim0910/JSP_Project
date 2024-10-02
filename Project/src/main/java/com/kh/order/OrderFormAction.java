102e9fca86ccc989e198190a950fb7395cc5c186
Beommm1330 <c26beom@gmail.com>
2024-10-02 12:23:57 +0900

    00

--- Project/src/main/java/com/kh/order/OrderFormAction.java
+++ Project/src/main/java/com/kh/order/OrderFormAction.java
@@ -1,7 +1,6 @@
 package com.kh.order;
 
 import com.kh.login.UserDTO;
-import com.kh.order.OrderDAO;
 import com.kh.web.action.Action;
 import com.kh.web.action.ActionForward;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,24 +10,26 @@ public class OrderFormAction implements Action{
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
-		
 		ActionForward forward = new ActionForward();
-		OrderDAO odao = new OrderDAO();
 		UserDTO order = new UserDTO();
-		
+
+		// 파라미터 가져오기
 		String user_name = request.getParameter("user_name");
 		String user_address = request.getParameter("user_address");
 		String user_phone = request.getParameter("user_phone");
-		
+
+		// UserDTO에 설정
 		order.setUser_name(user_name);
 		order.setUser_address(user_address);
 		order.setUser_phone(user_phone);
-		
-		odao.insertInfo(order);
-		
-		forward.setRedirect(true);
-		
+
+		// JSP로 데이터를 전달하기 위해 request에 UserDTO 추가
+		request.setAttribute("order", order);
+
+		// order_form으로 포워딩
+		forward.setPath("/order/order_form.or");
+		forward.setRedirect(false);  // forward 방식으로 이동
+
 		return forward;
 	}
-
 }
