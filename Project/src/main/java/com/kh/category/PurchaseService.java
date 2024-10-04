22c54142a1ee8fd32fe72a57bef1284e799b2b43
Beommm1330 <c26beom@gmail.com>
2024-10-04 16:49:49 +0900

    11

--- Project/src/main/java/com/kh/category/PurchaseService.java
+++ Project/src/main/java/com/kh/category/PurchaseService.java
@@ -18,7 +18,7 @@ public class PurchaseService {
         request.setAttribute("goods", goods);
         request.setAttribute("quantity", quantity);
         
-		forward.setRedirect(false);
+		forward.setRedirect(true);
 		forward.setPath("/order/order_form.jsp");
         
         return forward;
