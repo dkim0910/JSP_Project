af2ed9261ea047395ca14a58c5dc0a5ca519fc34
Beommm1330 <c26beom@gmail.com>
2024-10-07 10:14:14 +0900

    로그인/ 조인 쪽 1차 수정

--- Project/src/main/webapp/login/js/user.js
+++ Project/src/main/webapp/login/js/user.js
@@ -2,92 +2,99 @@
  * 
  */
 // 회원가입 폼 value 확인하는 자바스크립트
-function sendit(){
+function sendit() {
 	let frm = document.joinForm;
 	let userid = frm.userid;
 	let userpw = frm.userpw;
 	let userpw_re = frm.userpw_re;
 	let username = frm.username;
 	let userphone = frm.userphone;
-	
+
 	// 아이디 빈값 처리
-	if( userid.value == "" ){
+	if (userid.value == "") {
 		alert("아이디를 입력하세요");
 		userid.focus();
 		return false;
 	}
-	
+
 	// 아이디는 5자리 이상, 16자 미만
-	if( userid.value.length <= 4 
-			|| userid.value.length >= 16 ){
+	if (userid.value.length <= 4
+		|| userid.value.length >= 16) {
 		alert("아이디는 5자리 이상, 16자 미만으로 작성해주세요");
 		userid.focus();
 		return false;
 	}
-		
+
 	// 비밀번호 빈값 처리
-	if( userpw.value == "" ){
+	if (userpw.value == "") {
 		alert("패스워드를 입력하세요");
 		userpw.focus();
 		return false;
 	}
-	
+
 	// 비밀번호 8자리 이상 처리
-	if( userpw.value.length < 8 ){
+	if (userpw.value.length < 8) {
 		alert("비밀번호는 8자 이상으로 입력하세요");
 		userpw.focus();
 		return false;
 	}
-	
+
 	// 비밀번호 == 비밀번호 확인 처리
-	if( userpw.value != userpw_re.value ){
+	if (userpw.value != userpw_re.value) {
 		alert("비밀번호 확인이 다릅니다");
 		userpw_re.focus();
 		return false;
 	}
-	
+
 	// 이름 빈값 처리
-	if( username.value == "" ){
+	if (username.value == "") {
 		alert("이름을 입력하세요");
 		username.focus();
 		return false;
 	}
-	
+
 	// 휴대폰 번호 빈값처리
-	if( userphone.value == "" ){
+	if (userphone.value == "") {
 		alert("휴대폰 번호를 입력하세요");
 		userphone.focus();
 		return false;
 	}
-	
+
 	frm.submit();
 }
 
-function login(){
+function login() {
 	let frm = document.frm;
 	let userid = frm.userid;
 	let userpw = frm.userpw;
-	
+
 	// 아이디 빈값 체크
-	if(userid.value == ""){
+	if (userid.value == "") {
 		alert("아이디를 입력하세요!");
 		userid.focus();
 		return false;
 	}
-	
+
 	// 비밀번호 빈값 체크
-	if(userpw.value == ""){
+	if (userpw.value == "") {
 		alert("비밀번호를 입력하세요!");
 		userpw.focus();
 		return false;
-	}	
-	
+	}
+
+	// 입력한 아이디가 존재하지 않는 경우 체크
+	if (!existingUserIDs.includes(userid.value)) {
+		alert("존재하지 않는 아이디입니다!");
+		userid.focus();
+		return false;
+	}
+
 	frm.submit();
 }
 
-function checkId(){
+function checkId() {
 	let userid = document.getElementById("userid").value;
-	if( userid == "" ){
+	if (userid == "") {
 		alert("아이디를 입력해주세요");
 		userid.focus();
 		return false;
@@ -95,25 +102,25 @@ function checkId(){
 		// ajax 통신으로 id 중복체크
 		let xhr = new XMLHttpRequest();
 		xhr.open("POST", "/join/checkId.fr", true);
-		
+
 		// Content-Type 설정
 		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 		xhr.send("user_id=" + encodeURIComponent(userid));
-		xhr.onreadystatechange = function(){
-			if(xhr.status == 200 
-				&& xhr.readyState == XMLHttpRequest.DONE){
+		xhr.onreadystatechange = function() {
+			if (xhr.status == 200
+				&& xhr.readyState == XMLHttpRequest.DONE) {
 				// alert(xhr.responseText);	
-				if(xhr.responseText.trim() == "ok"){
+				if (xhr.responseText.trim() == "ok") {
 					document.getElementById("text").innerHTML
-					= "사용할 수 있는 아이디 입니다.";
+						= "사용할 수 있는 아이디 입니다.";
 				} else {
 					document.getElementById("text").innerHTML
-					= "중복된 아이디 입니다. ";
+						= "중복된 아이디 입니다. ";
 				}
 			}
 		}
-		
-	}	
+
+	}
 }
 
 
