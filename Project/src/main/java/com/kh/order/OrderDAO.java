60fff3e7177cfef1dc4d38c26d1f0bd2fb10a315
Beommm1330 <c26beom@gmail.com>
2024-10-11 09:12:07 +0900

    주석 삭제

--- Project/src/main/java/com/kh/order/OrderDAO.java
+++ Project/src/main/java/com/kh/order/OrderDAO.java
@@ -30,37 +30,6 @@ public class OrderDAO {
 		return result;
 	}
 
-//	public boolean insertOrderAndProducts(String user_id, String[] product_ids) {
-//		boolean result = true;
-//		SqlSession session = factory.openSession();
-//
-//		try {
-//			// 주문 번호 생성
-//			HashMap<String, String> orderData = new HashMap<>();
-//			orderData.put("user_id", user_id);
-//			session.insert("Order.insertOrder", orderData);
-//
-//			// 생성된 주문 번호 가져오기
-//			int orderedNum = session.selectOne("Order.getLastInsertedOrderNum"); // 가장 최근에 삽입된 주문 번호
-//
-//			for (String product_id : product_ids) {
-//				HashMap<String, String> data = new HashMap<>();
-//				data.put("ordered_num", String.valueOf(orderedNum));
-//				data.put("user_id", user_id);
-//				data.put("product_id", product_id);
-//
-//				int rowsAffected = session.insert("Order.insertOrderedList", data);
-//				if (rowsAffected <= 0) {
-//					result = false; // 하나라도 실패하면 false로 설정
-//				}
-//			}
-//		} finally {
-//			session.close();
-//		}
-//
-//		return result; // 모든 삽입이 성공했으면 true 반환
-//	}
-
 	public boolean insertOrderedList(String product_id, String user_id) {
 		boolean result = false;
 		HashMap<String, String> data = new HashMap<String, String>();
